43e435f1dff7aa7e5e99ed747c5eb5621d66693f
EGDY <ariasegdy@gmail.com>
2025-03-20 12:43:34 -0400

    fin del dia

--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::prefix('dashboard')->group(function(){
+    Route::get('contact', );
+
+});
+
 require __DIR__.'/auth.php';
